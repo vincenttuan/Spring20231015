67b979a4a9da8666a2e6d9ebb3170bfe61c21666
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-15 16:02:30 +0800

    java

--- SpringCore/src/main/java/spring/core/session01/test/TestHello.java
+++ SpringCore/src/main/java/spring/core/session01/test/TestHello.java
@@ -1,6 +1,10 @@
 package spring.core.session01.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import spring.core.session01.bean.Hello;
+import spring.core.session01.conf.JavaSpringConfig;
 
 public class TestHello {
 	
@@ -10,7 +14,12 @@ public class TestHello {
 		System.out.println(hello.today());
 		
 		// 利用 Spring 來管理 new 物件的問題
-		
+		// 使用 JavaSpringConfig (Java 配置) 來取得 bean 物件
+		// 1. 得到應用程式配置環境
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaSpringConfig.class);
+		// 2. 取 bean
+		Hello hello2 = ctx.getBean("hello", Hello.class);
+		System.out.println(hello2.today());
 		
 	}
 	
