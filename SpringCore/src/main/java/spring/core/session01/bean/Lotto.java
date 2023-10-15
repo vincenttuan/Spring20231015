8faac0875926fe47703e6072b779d9e393ea3589
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-15 16:54:15 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session01/bean/Lotto.java
@@ -0,0 +1,18 @@
+package spring.core.session01.bean;
+
+import java.util.Random;
+
+public class Lotto {
+	
+	private int number;
+	
+	public Lotto() {
+		Random random = new Random();
+		number = random.nextInt(100); // 0~99
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+}
