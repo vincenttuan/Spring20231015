f99ac2b4da58b74fb491e75c575b41b9ac016d83
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-15 15:49:16 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session01/test/TestHello.java
@@ -0,0 +1,15 @@
+package spring.core.session01.test;
+
+import spring.core.session01.bean.Hello;
+
+public class TestHello {
+	
+	public static void main(String[] args) {
+		// 傳統方式
+		Hello hello = new Hello();
+		System.out.println(hello.today());
+		
+		
+	}
+	
+}
