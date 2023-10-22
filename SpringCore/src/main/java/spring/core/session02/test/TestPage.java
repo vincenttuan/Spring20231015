ad9f0060902e0ebcbfe4b3196430ac4057286f0f
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-22 15:51:34 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session02/test/TestPage.java
@@ -0,0 +1,21 @@
+package spring.core.session02.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session02.bean.Paper;
+import spring.core.session02.conf.JavaSpringConfig2;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config2.xml");
+		//ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaSpringConfig2.class);
+		
+		Paper paper1 = ctx.getBean("paper1", Paper.class);
+		System.out.println("paper1: " + paper1);
+		
+	}
+
+}
