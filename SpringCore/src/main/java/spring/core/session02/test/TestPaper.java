b500f36b8c7efe89e9bfe0ed511639e6db42279f
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-22 16:57:57 +0800

    java

--- SpringCore/src/main/java/spring/core/session02/test/TestPaper.java
+++ SpringCore/src/main/java/spring/core/session02/test/TestPaper.java
@@ -13,8 +13,8 @@ public class TestPaper {
 		//ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config2.xml");
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaSpringConfig2.class);
 		
-		Paper paper1 = ctx.getBean("paper1", Paper.class);
-		System.out.println("paper1: " + paper1);
+		Paper paper1 = ctx.getBean("paper1", Paper.class); // 若使用 xml 配置該行要拿掉, 因為會受到 byType 的限制
+		System.out.println("paper1: " + paper1); // 若使用 xml 配置該行要拿掉, 因為會受到 byType 的限制
 		
 		Paper paper2 = ctx.getBean("paper2", Paper.class);
 		System.out.println("paper2: " + paper2);
