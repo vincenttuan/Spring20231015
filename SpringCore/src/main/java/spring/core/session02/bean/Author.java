b2091419963126e82e91e3cc8acde7906885b4bc
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-22 13:29:04 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session02/bean/Author.java
@@ -0,0 +1,44 @@
+package spring.core.session02.bean;
+
+public class Author {
+	private String name;
+	private Character sex;
+	private Integer age;
+	
+	public Author() {
+		
+	}
+
+	public Author(String name, Character sex, Integer age) {
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Character getSex() {
+		return sex;
+	}
+
+	public void setSex(Character sex) {
+		this.sex = sex;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+}
