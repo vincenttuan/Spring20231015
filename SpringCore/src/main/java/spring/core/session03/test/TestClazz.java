e3bb9880ff8c4c19abdc9797173d62e182759805
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-29 14:23:41 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/test/TestClazz.java
+++ SpringCore/src/main/java/spring/core/session03/test/TestClazz.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session03.bean.Clazz;
 import spring.core.session03.bean.Student;
+import spring.core.session03.bean.Teacher;
 
 public class TestClazz {
 
@@ -18,7 +19,12 @@ public class TestClazz {
 		Student student2 = ctx.getBean("student2", Student.class);
 		System.out.println(student1);
 		System.out.println(student2);
-		
+		// 講師
+		Teacher teacher1 = ctx.getBean("teacher1", Teacher.class);
+		System.out.println(teacher1);
+		System.out.println(teacher1.getStudents());
+		System.out.println(teacher1.getSubjects());
+		System.out.println(teacher1.getSalary());
 	}
 
 }
