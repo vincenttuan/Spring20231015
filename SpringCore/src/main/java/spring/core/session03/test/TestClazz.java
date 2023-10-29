7fc17227876c4cd20ccc0cbe28785fd9f862dc95
段浩恩 <@DESKTOP-7JSLF7F>
2023-10-29 14:01:30 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/test/TestClazz.java
+++ SpringCore/src/main/java/spring/core/session03/test/TestClazz.java
@@ -4,14 +4,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session03.bean.Clazz;
+import spring.core.session03.bean.Student;
 
 public class TestClazz {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		// 課程
 		Clazz clazz1 = ctx.getBean("clazz1", Clazz.class);
 		System.out.println(clazz1);
-
+		// 學生
+		Student student1 = ctx.getBean("student1", Student.class);
+		Student student2 = ctx.getBean("student2", Student.class);
+		System.out.println(student1);
+		System.out.println(student2);
+		
 	}
 
 }
