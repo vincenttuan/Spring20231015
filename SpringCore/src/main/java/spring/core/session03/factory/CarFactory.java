1899ef20dedf7695c2c543ee9a72a092a8def2cf
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 13:24:25 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/factory/CarFactory.java
+++ SpringCore/src/main/java/spring/core/session03/factory/CarFactory.java
@@ -12,6 +12,9 @@ import spring.core.session03.bean.Car;
  * 例如:
  * 1. 建立一個 bean 物件的時候, 必須要連資料庫取得相關資訊之後, 將該資訊注入到指定物件屬性中
  * 2. 建立一個 bean 物件的時候, 必須要連某一個雲端資源取得相關資訊之後, 將該資訊注入到指定物件屬性中
+ * 情境: 有一個天氣物件 Weather
+ * 當使用者要取得天氣物件的時候, 該物件在建立的時期會先到 https://openweathermap.org/api 透過 API 取得目前的天氣資料
+ * 此時因為建立天氣物件的時候過程具有複雜性, 所以適合使用 FactoryBean 來管理此物件
  * */
 public class CarFactory implements FactoryBean<Car> {
 
