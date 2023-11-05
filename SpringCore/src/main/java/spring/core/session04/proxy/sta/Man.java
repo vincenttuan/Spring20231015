5ced603ff321f58cc0a7e21f9e651bcb0071d4ef
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 16:43:37 +0800

    java

--- SpringCore/src/main/java/spring/core/session04/proxy/sta/Man.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/sta/Man.java
@@ -6,6 +6,8 @@ public class Man implements Person {
 	public void work() {
 		// 業務邏輯
 		System.out.println("Man 上班工作");
+		// 模擬執行過程中發生例外 ...
+		throw new RuntimeException("口罩突然不見了");
 		// 其他業務邏輯 ...
 	}
 	
