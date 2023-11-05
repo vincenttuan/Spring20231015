ccdbbc75a7bd4b3a1edc0a50deeef2e8ebf5d4f9
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 13:21:03 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/factory/CarFactory.java
+++ SpringCore/src/main/java/spring/core/session03/factory/CarFactory.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.FactoryBean;
 
 import spring.core.session03.bean.Car;
 
+/*
+ * FactoryBean 的使用時機:
+ * 若需要對 bean 做一系列的複雜初始程序
+ * 例如:
+ * 1. 建立一個 bean 物件的時候, 必須要連資料庫取得相關資訊之後, 將該資訊注入到指定物件屬性中
+ * 2. 建立一個 bean 物件的時候, 必須要連某一個雲端資源取得相關資訊之後, 將該資訊注入到指定物件屬性中
+ * */
 public class CarFactory implements FactoryBean<Car> {
 
 	@Override
