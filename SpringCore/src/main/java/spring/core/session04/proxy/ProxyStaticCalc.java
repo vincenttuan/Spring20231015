3ffe140980e319cd364229eae4bdcd4191b2eea8
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 17:01:12 +0800

    java

--- SpringCore/src/main/java/spring/core/session04/proxy/ProxyStaticCalc.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/ProxyStaticCalc.java
@@ -8,7 +8,9 @@ public class ProxyStaticCalc {
 
 	public static void main(String[] args) {
 		//Calc calc = new CalcImpl();
-		Calc calc = new CalcProxy(new CalcImpl());
+		//Calc calc = new CalcProxy(new CalcImpl());
+		
+		Calc calc = new CalcProxy((x, y) -> x / y);
 		System.out.println(calc.div(10, 2));
 		System.out.println(calc.div(10, 5));
 		System.out.println(calc.div(10, 0));
--- SpringCore/src/main/java/spring/core/session04/proxy/sta/Calc.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/sta/Calc.java
@@ -1,5 +1,6 @@
 package spring.core.session04.proxy.sta;
 
+@FunctionalInterface
 public interface Calc {
 	Integer div(Integer x, Integer y); // 除法計算
 }
