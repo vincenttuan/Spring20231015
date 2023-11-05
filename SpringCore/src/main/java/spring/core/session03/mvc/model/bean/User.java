703ae251c760c18dcf31d5dbce376de75617ae7c
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 15:11:47 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/mvc/model/bean/User.java
+++ SpringCore/src/main/java/spring/core/session03/mvc/model/bean/User.java
@@ -5,14 +5,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component // 等於 @Component(value = "user")
 public class User {
+	
+	@Value(value = "John") // 給予屬性預設值
 	private String username; // 姓名
 	
+	@Value(value = "19")
 	private Integer age; // 年齡
 	
+	/*
+	 * #{} 這是一個 SpringEL 表達式
+	 * ${ .. } 表示取得屬性值. Ex: ${nickname} 表示要取得/配置 nickname 的資料
+	 * ${nickname: {'Baby', 'Lucky'}} 使用 "Baby", "Lucky" 作為 nickname 的預設陣列資料
+	 * */
+	@Value(value = "#{${nickname: {'Baby', 'Lucky'}}}")
 	private String[] nickname; // 暱稱
 	
 	private Set<String> subjects; // 所修的科目
