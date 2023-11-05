6b4e8b3d8bab45d68b6a0fcca97796bb91ac0b6c
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 14:02:58 +0800

    java

--- SpringCore/src/main/java/spring/core/session03/mvc/model/bean/User.java
+++ SpringCore/src/main/java/spring/core/session03/mvc/model/bean/User.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.stereotype.Component;
+
+@Component // 等於 @Component(value = "user")
 public class User {
 	private String username; // 姓名
 	
