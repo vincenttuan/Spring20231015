62cdb5f131028689261ff4e18ca3555a87107950
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-05 16:28:35 +0800

    java

--- SpringCore/src/main/java/spring/core/session04/proxy/ProxyStaticDemo.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/ProxyStaticDemo.java
@@ -3,15 +3,24 @@ package spring.core.session04.proxy;
 import spring.core.session04.proxy.sta.Child;
 import spring.core.session04.proxy.sta.Man;
 import spring.core.session04.proxy.sta.Person;
+import spring.core.session04.proxy.sta.PersonProxy;
 import spring.core.session04.proxy.sta.Woman;
 
 public class ProxyStaticDemo {
 
 	public static void main(String[] args) {
-		// 不透過代理直接執行
+		// 不透過代理直接創建
+		/*
 		Person man = new Man();
 		Person woman = new Woman();
 		Person child = new Child();
+		*/
+		
+		// 透過代理間接創建
+		Person man = new PersonProxy(new Man());
+		Person woman = new PersonProxy(new Woman());
+		Person child = new PersonProxy(new Child());
+		
 		man.work();
 		woman.work();
 		child.work();
