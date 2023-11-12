61c66f0a49ca8ff5ccb1e201cdda9e0007d4d661
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-12 15:34:10 +0800

    java

--- SpringCore/src/main/java/spring/core/session05/aop/test/AOPTest.java
+++ SpringCore/src/main/java/spring/core/session05/aop/test/AOPTest.java
@@ -6,6 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.core.session05.aop.Calc;
 import spring.core.session05.aop.CalcImpl;
 
+// 執行時要加入: JVM 啟動參數
+// --add-opens java.base/java.lang=ALL-UNNAMED
+// 允許 Java 9 以上版本使用反射(java.lang.reflect)內部 API
 public class AOPTest {
 
 	public static void main(String[] args) {
