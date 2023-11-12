bba6e5069668a8c3d98a08dc215c8487cad21e76
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-12 13:29:15 +0800

    java

--- SpringCore/src/main/java/spring/core/session04/proxy/dyn/CalcImpl.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/dyn/CalcImpl.java
@@ -5,12 +5,14 @@ public class CalcImpl implements Calc {
 
 	@Override
 	public int add(int x, int y) {
+		// 業務邏輯
 		int result = x + y;
 		return result;
 	}
 
 	@Override
 	public int div(int x, int y) {
+		// 業務邏輯
 		int result = x / y;
 		return result;
 	}
