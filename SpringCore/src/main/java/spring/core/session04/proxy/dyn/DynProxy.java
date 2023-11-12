ba6a34dfa4a6ffa0fdb86c9a619086413e4139aa
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-12 13:22:15 +0800

    java

--- SpringCore/src/main/java/spring/core/session04/proxy/dyn/DynProxy.java
+++ SpringCore/src/main/java/spring/core/session04/proxy/dyn/DynProxy.java
@@ -1,5 +1,9 @@
 package spring.core.session04.proxy.dyn;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 // 動態代理
 public class DynProxy {
 	
@@ -14,8 +18,24 @@ public class DynProxy {
 	public Object getProxy() {
 		Object proxyObj = null;
 		
+		// 1. 載入類別器
+		ClassLoader loader = object.getClass().getClassLoader();
 		
+		// 2. 被代理的物件所實作的介面
+		Class<?>[] interfaces = object.getClass().getInterfaces();
 		
+		// 3. 處理代理的實現
+		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			Object result = null;
+			
+			// 調用被代理物件的業務方法
+			result = method.invoke(object, args); // object 被代理的物件, args 方法參數
+			
+			return result;
+		};
+		
+		// 4. 取得代理的物件
+		proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
 		return proxyObj;
 	}
 	
