79f67cc2a7af765ebf0ca524307674b0c9d2eded
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-12 16:13:43 +0800

    java

--- SpringCore/src/main/java/spring/core/session05/aop/MyLoggerAspect.java
+++ SpringCore/src/main/java/spring/core/session05/aop/MyLoggerAspect.java
@@ -24,6 +24,9 @@ public class MyLoggerAspect {
 	@Pointcut(value = "execution(public Integer spring.core.session05.aop.CalcImpl.div(..))")
 	public void pt2() {}
 	
+	@Pointcut(value = "execution(public Integer spring.core.session05.aop.CalcImpl.*(..))")
+	public void pt3() {}
+	
 	// 前置通知(Advice)
 	//@Before(value = "execution(public Integer spring.core.session05.aop.CalcImpl.add(Integer, Integer))") // 注入指定方法簽章
 	//@Before(value = "execution(public Integer spring.core.session05.aop.CalcImpl.add(..))") // 注入指定方法簽章, 不限制方法參數
@@ -31,7 +34,10 @@ public class MyLoggerAspect {
 	//@Before(value = "execution(* spring.core.session05.aop.CalcImpl.*(..))") // 不限權限, 不限回傳值, 不指定方法名稱, 不限制方法參數
 	//@Before(value = "execution(* spring.core.session05.aop.*.*(..))") // 不限權限, 不限回傳值, 不限類別, 不指定方法名稱, 不限制方法參數
 	//@Before(value = "execution(* *(..))") // 全部攔截, 都不限制
-	@Before(value = "pt()")
+	//@Before(value = "pt()")
+	//@Before(value = "pt2()")
+	//@Before(value = "pt3()")
+	@Before(value = "pt3() && !pt2()") // 切入點表達式支援邏輯運算子: &&, ||, !
 	public void beforeAdvice(JoinPoint joinPoint) { // JoinPoint 連接點
 		String methodName = joinPoint.getSignature().getName(); // 得到 JoinPoint 連接點的方法名稱
 		Object[] args = joinPoint.getArgs(); // 得到 JoinPoint 連接點的方法參數
