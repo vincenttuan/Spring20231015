db5f829247267b57445be7a54570de0818dbb959
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-12 13:13:28 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session04/proxy/dyn/DynProxy.java
@@ -0,0 +1,22 @@
+package spring.core.session04.proxy.dyn;
+
+// 動態代理
+public class DynProxy {
+	
+	// 被代理的對象(物件)
+	private Object object;
+	
+	public DynProxy(Object object) {
+		this.object = object;
+	}
+	
+	// 取得被代理的物件
+	public Object getProxy() {
+		Object proxyObj = null;
+		
+		
+		
+		return proxyObj;
+	}
+	
+}
