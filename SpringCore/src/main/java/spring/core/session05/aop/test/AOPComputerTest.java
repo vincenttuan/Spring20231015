76cf4ee5e531863ef1d761c60cbf91d3351e4d01
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-19 13:10:47 +0800

    java

--- SpringCore/src/main/java/spring/core/session05/aop/test/AOPComputerTest.java
+++ SpringCore/src/main/java/spring/core/session05/aop/test/AOPComputerTest.java
@@ -1,9 +1,18 @@
 package spring.core.session05.aop.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session05.aop.Computer;
+import spring.core.session05.aop.ComputerImpl;
+
 public class AOPComputerTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config5-aop.xml");
+		Computer computer = ctx.getBean("computerImpl", ComputerImpl.class);
+		System.out.println(computer.add(10, 20));
+		System.out.println(computer.div(40, 20));
 
 	}
 
