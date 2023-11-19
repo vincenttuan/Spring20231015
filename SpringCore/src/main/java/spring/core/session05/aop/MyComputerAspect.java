3282d11cec4275001979697212edca20279d391d
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-19 13:27:46 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session05/aop/MyComputerAspect.java
@@ -0,0 +1,48 @@
+package spring.core.session05.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(2)
+public class MyComputerAspect {
+	@Pointcut(value = "execution(* spring.core.session05.aop.ComputerImpl.*(..))")
+	public void pt() {}
+	
+	// 環繞通知
+	// 注意事項: 若要進行環繞通知, 建議將其他通知關閉
+	@Around(value = "pt()")
+	public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		// 建立返回值變數
+		Object result = null;
+		try {
+			// 1. 前置通知
+			String methodName = proceedingJoinPoint.getSignature().getName();
+			Object[] args = proceedingJoinPoint.getArgs();
+			System.out.printf("環繞通知(前置通知) - 方法名稱: %s 方法參數: %s%n", methodName, Arrays.toString(args));
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		return result;
+	}
+}
+
+
+
+
+
+
+
+
+
+
