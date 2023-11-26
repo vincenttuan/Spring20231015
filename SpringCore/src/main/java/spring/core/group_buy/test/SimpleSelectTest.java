012de9c698b018b5afe9daaa135207f007f27cac
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-26 14:35:18 +0800

    java

--- SpringCore/src/main/java/spring/core/group_buy/test/SimpleSelectTest.java
+++ SpringCore/src/main/java/spring/core/group_buy/test/SimpleSelectTest.java
@@ -1,10 +1,18 @@
 package spring.core.group_buy.test;
 
+import java.util.List;
+
+import spring.core.group_buy.dao.GroupBuyDao;
+import spring.core.group_buy.dao.GroupBuyDaoMySQL;
+import spring.core.group_buy.entity.User;
+
 public class SimpleSelectTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GroupBuyDao dao = new GroupBuyDaoMySQL();
+		List<User> users = dao.findAllUsers();
+		System.out.println(users.size());
+		System.out.println(users);
 	}
 
 }
