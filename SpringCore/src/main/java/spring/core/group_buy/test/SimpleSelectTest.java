c22630916bcfa645cb64d9bc8dfd818743576d22
段浩恩 <@DESKTOP-7JSLF7F>
2023-11-26 15:53:21 +0800

    java

--- SpringCore/src/main/java/spring/core/group_buy/test/SimpleSelectTest.java
+++ SpringCore/src/main/java/spring/core/group_buy/test/SimpleSelectTest.java
@@ -3,8 +3,10 @@ package spring.core.group_buy.test;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.group_buy.conf.GroupBuyConfig;
 import spring.core.group_buy.dao.GroupBuyDao;
 import spring.core.group_buy.dao.GroupBuyDaoMySQL;
 import spring.core.group_buy.entity.User;
@@ -19,7 +21,8 @@ public class SimpleSelectTest {
 		System.out.println(users);
 		*/
 		
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config-group-buy.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config-group-buy.xml");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(GroupBuyConfig.class);
 		GroupBuyDao dao = ctx.getBean("groupBuyDaoMySQL", GroupBuyDaoMySQL.class);
 		List<User> users = dao.findAllUsers();
 		System.out.println(users.size());
