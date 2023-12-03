904dca15faf5d05f1373e77fd01dd9aba582c066
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-03 14:05:42 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session08/controller/HelloController.java
+++ SpringMVC/src/main/java/spring/mvc/session08/controller/HelloController.java
@@ -3,6 +3,7 @@ package spring.mvc.session08.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // Base url      : http://localhost:8080/SpringMVC/mvc
@@ -19,6 +20,21 @@ public class HelloController {
 	@ResponseBody
 	public String welcome() {
 		return "Welcome Spring MVC!";
+	}
+	
+	/*
+	 * 2. ? 帶參數
+	 * 網址: http://localhost:8080/SpringMVC/mvc/hello/sayhi?name=John&age=20
+	 * 限制: name 參數是一定要有的(預設), 
+	 *      age 參數是不一定要有的, 初始值 = 0
+	 * */
+	@GetMapping(value = "/sayhi")
+	@ResponseBody
+	public String sayhi(@RequestParam(value = "name", required = true) String name,
+						@RequestParam(value = "age", required = false, defaultValue = "0") Integer age) {
+		
+		String message = String.format("Hi %s, %d", name, age);
+		return message;
 	}
 	
 }
