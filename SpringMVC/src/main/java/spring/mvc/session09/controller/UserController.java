2b11aa6e5633d3937d94b370cc6de7885643f0be
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 15:27:55 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
+++ SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.mvc.session09.dao.DataDao;
 import spring.mvc.session09.dao.UserDao;
@@ -36,5 +38,10 @@ public class UserController {
 		return "session09/user";
 	}
 	
+	@PostMapping("/")
+	@ResponseBody
+	public String add(@ModelAttribute User user) {
+		return user.toString();
+	}
 	
 }
