1fd18fa11948107290073174937cb6a6e487272f
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 15:39:16 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/entity/User.java
+++ SpringMVC/src/main/java/spring/mvc/session09/entity/User.java
@@ -16,99 +16,93 @@ public class User {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Date birth; // 生日
-	
-	private EducationData education; // 教育程度: 1:小學, 2:國中, 3:高中, 4:大學, 5:研究所 (單選)
-	private SexData sex;       // 性別: 1:男生, 2:女生 (單選)
-	private List<InterestData> interests; // 興趣: 1:爬山, 2:看書, 3:打球, 4:飛控, 5:手遊 (多選)
-	
 	private String resume; // 履歷
 	
-	public User() {
-		
-	}
+	private Integer educationId;
+	private EducationData education; // 教育程度: 1:小學, 2:國中, 3:高中, 4:大學, 5:研究所 (單選)
 	
-	public User(Integer id, String name, Integer age, Date birth, EducationData education, SexData sex,
-			List<InterestData> interests, String resume) {
-		this.id = id;
-		this.name = name;
-		this.age = age;
-		this.birth = birth;
-		this.education = education;
-		this.sex = sex;
-		this.interests = interests;
-		this.resume = resume;
-	}
-
+	private Integer sexId;
+	private SexData sex;       // 性別: 1:男生, 2:女生 (單選)
+	
+	private Integer[] interestIds;
+	private List<InterestData> interests; // 興趣: 1:爬山, 2:看書, 3:打球, 4:飛控, 5:手遊 (多選)
+	
+	// -----------------------------------------------------------------------------------
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Integer getAge() {
 		return age;
 	}
-
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-
 	public Date getBirth() {
 		return birth;
 	}
-
 	public void setBirth(Date birth) {
 		this.birth = birth;
 	}
-
-	public EducationData getEducation() {
-		return education;
-	}
-
-	public void setEducation(EducationData education) {
-		this.education = education;
-	}
-
-	public SexData getSex() {
-		return sex;
-	}
-
-	public void setSex(SexData sex) {
-		this.sex = sex;
-	}
-
-	public List<InterestData> getInterests() {
-		return interests;
-	}
-
-	public void setInterests(List<InterestData> interests) {
-		this.interests = interests;
-	}
-
 	public String getResume() {
 		return resume;
 	}
-
 	public void setResume(String resume) {
 		this.resume = resume;
 	}
-
+	public Integer getEducationId() {
+		return educationId;
+	}
+	public void setEducationId(Integer educationId) {
+		this.educationId = educationId;
+	}
+	public EducationData getEducation() {
+		return education;
+	}
+	public void setEducation(EducationData education) {
+		this.education = education;
+	}
+	public Integer getSexId() {
+		return sexId;
+	}
+	public void setSexId(Integer sexId) {
+		this.sexId = sexId;
+	}
+	public SexData getSex() {
+		return sex;
+	}
+	public void setSex(SexData sex) {
+		this.sex = sex;
+	}
+	public Integer[] getInterestIds() {
+		return interestIds;
+	}
+	public void setInterestIds(Integer[] interestIds) {
+		this.interestIds = interestIds;
+	}
+	public List<InterestData> getInterests() {
+		return interests;
+	}
+	public void setInterests(List<InterestData> interests) {
+		this.interests = interests;
+	}
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", age=" + age + ", birth=" + birth + ", education=" + education
-				+ ", sex=" + sex + ", interests=" + interests + ", resume=" + resume + "]";
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", birth=" + birth + ", resume=" + resume
+				+ ", educationId=" + educationId + ", education=" + education + ", sexId=" + sexId + ", sex=" + sex
+				+ ", interestIds=" + Arrays.toString(interestIds) + ", interests=" + interests + "]";
 	}
-
+	
+	
+	
 	
 	
 }
