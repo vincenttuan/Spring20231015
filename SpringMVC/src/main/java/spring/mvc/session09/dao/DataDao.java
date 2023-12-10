9de3fc6c2824ab5210c92d858f8038bb7ee8e5ed
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 15:57:37 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/dao/DataDao.java
+++ SpringMVC/src/main/java/spring/mvc/session09/dao/DataDao.java
@@ -1,6 +1,7 @@
 package spring.mvc.session09.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import spring.mvc.session09.entity.EducationData;
 import spring.mvc.session09.entity.InterestData;
@@ -8,6 +9,12 @@ import spring.mvc.session09.entity.SexData;
 
 public interface DataDao {
 	List<EducationData> findAllEducationDatas();
-	List<InterestData> findAllInterestDatas();
+	Optional<EducationData> getEducationDataById(Integer id);
+	
 	List<SexData> findAllSexDatas();
+	Optional<SexData> getSexDataById(Integer id);
+	
+	List<InterestData> findAllInterestDatas();
+	List<InterestData> findInterestDatasByIds(Integer... ids);
+	
 }
