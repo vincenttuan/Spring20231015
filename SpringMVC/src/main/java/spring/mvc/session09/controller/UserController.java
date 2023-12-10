45cc439401a7746ae3a18998cb9c749b7e13d2ed
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 14:18:37 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
+++ SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
@@ -1,12 +1,23 @@
 package spring.mvc.session09.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.mvc.session09.entity.User;
 
 @Controller
 @RequestMapping("/session09/user")
 public class UserController {
 	
+	// 首頁
+	@GetMapping("/")
+	public String index(@ModelAttribute User user, Model model) {
+		
+		return "session09/user";
+	}
 	
 	
 }
