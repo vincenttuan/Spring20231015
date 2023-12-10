229d22691bd811523450eda5e066b3f32feb92e9
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 16:08:14 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
+++ SpringMVC/src/main/java/spring/mvc/session09/controller/UserController.java
@@ -1,5 +1,8 @@
 package spring.mvc.session09.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.mvc.session09.dao.DataDao;
 import spring.mvc.session09.dao.UserDao;
+import spring.mvc.session09.entity.EducationData;
+import spring.mvc.session09.entity.InterestData;
+import spring.mvc.session09.entity.SexData;
 import spring.mvc.session09.entity.User;
 
 @Controller
@@ -41,6 +47,21 @@ public class UserController {
 	@PostMapping("/")
 	@ResponseBody
 	public String add(@ModelAttribute User user) {
+		// 根據 id 找到符合的物件
+		Optional<EducationData> eduOpt = dataDao.getEducationDataById(user.getEducationId());
+		Optional<SexData> sexOpt = dataDao.getSexDataById(user.getSexId());
+		List<InterestData> interestDatas = dataDao.findInterestDatasByIds(user.getInterestIds());
+		// 注入
+		if(eduOpt.isPresent()) {
+			user.setEducation(eduOpt.get());
+		}
+		
+		if(sexOpt.isPresent()) {
+			user.setSex(sexOpt.get());
+		}
+		
+		user.setInterests(interestDatas);
+		
 		return user.toString();
 	}
 	
