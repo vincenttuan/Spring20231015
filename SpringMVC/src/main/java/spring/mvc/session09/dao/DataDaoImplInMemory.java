2343385a0424024d3ada20d595cafbb54551dc20
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-10 16:03:55 +0800

    java

--- SpringMVC/src/main/java/spring/mvc/session09/dao/DataDaoImplInMemory.java
+++ SpringMVC/src/main/java/spring/mvc/session09/dao/DataDaoImplInMemory.java
@@ -1,7 +1,11 @@
 package spring.mvc.session09.dao;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
@@ -23,6 +27,27 @@ public class DataDaoImplInMemory implements DataDao {
 				new EducationData(5, "研究所"));
 		return educationDatas;
 	}
+	
+	@Override
+	public Optional<EducationData> getEducationDataById(Integer id) {
+		List<EducationData> educationDatas = findAllEducationDatas();
+		return educationDatas.stream().filter(edu -> edu.getId().equals(id)).findFirst();
+	}
+
+	@Override
+	public List<SexData> findAllSexDatas() {
+		// 性別: 1:男生, 2:女生
+		List<SexData> sexDatas = Arrays.asList(
+				new SexData(1, "男生"), 
+				new SexData(2, "女生"));
+		return sexDatas;
+	}
+
+	@Override
+	public Optional<SexData> getSexDataById(Integer id) {
+		List<SexData> sexDatas = findAllSexDatas();
+		return sexDatas.stream().filter(sex -> sex.getId().equals(id)).findFirst();
+	}
 
 	@Override
 	public List<InterestData> findAllInterestDatas() {
@@ -37,12 +62,13 @@ public class DataDaoImplInMemory implements DataDao {
 	}
 
 	@Override
-	public List<SexData> findAllSexDatas() {
-		// 性別: 1:男生, 2:女生
-		List<SexData> sexDatas = Arrays.asList(
-				new SexData(1, "男生"), 
-				new SexData(2, "女生"));
-		return sexDatas;
+	public List<InterestData> findInterestDatasByIds(Integer... ids) {
+		// 將 ids 轉 Set
+		Set<Integer> idSet = new HashSet<>(Arrays.asList(ids));
+		List<InterestData> interestDatas = findAllInterestDatas();
+		return interestDatas.stream()
+				.filter(interest -> idSet.contains(interest.getId()))
+				.collect(Collectors.toList());
 	}
 	
 }
