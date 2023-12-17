683b3fba2a64dc55ea5ab57acfc9448e558adff3
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-17 17:51:20 +0800

    java

--- SpringCore/src/main/java/spring/core/session06/tx/service/BookOneServiceImpl.java
+++ SpringCore/src/main/java/spring/core/session06/tx/service/BookOneServiceImpl.java
@@ -2,6 +2,7 @@ package spring.core.session06.tx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
