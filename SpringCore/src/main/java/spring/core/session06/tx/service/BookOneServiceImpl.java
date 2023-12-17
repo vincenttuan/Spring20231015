da145448724272ddd423e1839c96d6d9d5cf15d1
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-17 17:04:54 +0800

    java

--- SpringCore/src/main/java/spring/core/session06/tx/service/BookOneServiceImpl.java
+++ SpringCore/src/main/java/spring/core/session06/tx/service/BookOneServiceImpl.java
@@ -23,12 +23,16 @@ public class BookOneServiceImpl implements BookOneService {
 		writeToLog();
 		// 查 books
 		findAllBooks();
+		//--------------------------------------------------
 		// 1. 查詢書本價格
 		Integer bookPrice = bookDao.getBookPrice(bookId);
 		// 2. 減去書本庫存
 		bookDao.reduceBookStock(bookId, 1); // 固定 1
 		// 3. 修改客戶餘額
 		bookDao.reduceWalletBalance(username, bookPrice);
+		//--------------------------------------------------
+		updateBookStockNested(bookId, 1);
+		
 	}
 	
 	@Transactional(
@@ -48,6 +52,8 @@ public class BookOneServiceImpl implements BookOneService {
 	}
 	
 	@Transactional(propagation = Propagation.NESTED)
+	// 這意味著當它被一個已經存在的事務調用時，它將在一個嵌套的事務中執行。
+	// 如果這個方法內部發生異常導致需要回滾，只有這個嵌套事務會被回滾，而不會影響到外部的主事務。
     public void updateBookStockNested(Integer bookId, Integer stockReduction) {
         // 這個方法將在嵌套事務中執行
         try {
