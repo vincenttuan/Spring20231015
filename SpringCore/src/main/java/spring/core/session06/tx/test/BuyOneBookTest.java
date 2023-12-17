c1cc36eea2530942e6af1a52a5abdc7c73a52037
段浩恩 <@DESKTOP-7JSLF7F>
2023-12-17 15:08:11 +0800

    java

---
+++ SpringCore/src/main/java/spring/core/session06/tx/test/BuyOneBookTest.java
@@ -0,0 +1,16 @@
+package spring.core.session06.tx.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session06.tx.controller.BookController;
+
+public class BuyOneBookTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config-tx.xml");
+		BookController bookController = ctx.getBean(BookController.class);
+		bookController.buyOneBook("John", 1);
+	}
+
+}
