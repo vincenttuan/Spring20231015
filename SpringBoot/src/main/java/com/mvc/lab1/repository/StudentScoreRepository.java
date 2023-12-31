aaf8b4a5b2cc1f915b8e5c4a273c851a9a6b58e8
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-31 14:24:13 +0800

    java

--- SpringBoot/src/main/java/com/mvc/lab1/repository/StudentScoreRepository.java
+++ SpringBoot/src/main/java/com/mvc/lab1/repository/StudentScoreRepository.java
@@ -1,16 +1,17 @@
 package com.mvc.lab1.repository;
 
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mvc.lab1.entity.StudentScore;
 
 public interface StudentScoreRepository extends JpaRepository<StudentScore, Integer> {
-	// 只要修改名字
-	@Transactional
-	@Modifying
-	@Query("UPDATE StudentScore s SET s.name = :name WHERE s.id = :id")
-	void updateNameById(Integer id, String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE StudentScore s SET s.name = :name WHERE s.id = :id")
+    void updateNameById(@Param("id") Integer id, @Param("name") String name);
 }
