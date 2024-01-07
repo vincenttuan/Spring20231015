4473b6769050946704b8b687bd24613759f747bf
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 15:31:46 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/controller/DepartmentController.java
+++ SpringBootPSI/src/main/java/com/psi/controller/DepartmentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,7 @@ import com.psi.dto.DepartmentPageDTO;
 import com.psi.service.DepartmentService;
 
 @Controller
-@Repository("/department")
+@RequestMapping("/department")
 public class DepartmentController {
 	
 	@Autowired
