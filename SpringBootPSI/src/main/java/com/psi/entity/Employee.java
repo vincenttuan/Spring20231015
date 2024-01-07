3b825d0ddccd46b87ff52a8d0f8f88e4046bf6ac
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 13:40:38 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/entity/Employee.java
+++ SpringBootPSI/src/main/java/com/psi/entity/Employee.java
@@ -1,5 +1,24 @@
 package com.psi.entity;
 
-public class Employee {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 員工序號
+	
+	@Column(name = "name", unique = true, nullable = false, length = 50)
+	private String name; // 員工姓名
+	
 }
