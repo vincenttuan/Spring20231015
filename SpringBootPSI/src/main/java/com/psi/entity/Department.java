fda7fd477e54a1019960982b8c831d6889744e9e
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 13:57:20 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/psi/entity/Department.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -43,6 +44,7 @@ public class Department {
 	@OneToMany(mappedBy = "department", 
 			fetch = FetchType.LAZY,
 			orphanRemoval = false)
+	@OrderBy("id ASC")
 	private Set<Employee> employees = new LinkedHashSet<>(); 
 	
 }
