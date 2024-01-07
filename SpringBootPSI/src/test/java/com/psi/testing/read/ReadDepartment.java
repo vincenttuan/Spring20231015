6a784da0ced677cd527b8e4d2b88f159279894cd
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 21:10:38 +0800

    java

---
+++ SpringBootPSI/src/test/java/com/psi/testing/read/ReadDepartment.java
@@ -0,0 +1,27 @@
+package com.psi.testing.read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.psi.dto.DepartmentPageDTO;
+import com.psi.service.DepartmentService;
+
+@SpringBootTest
+public class ReadDepartment {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@Test
+	public void test() {
+		Pageable pageable = PageRequest.of(0, 10);
+		DepartmentPageDTO dto = departmentService.findAllDepartments(pageable);
+		dto.getDepartments().forEach(dept -> System.out.println(dept.getName()));
+		System.out.println(dto.getCurrentPage());
+		System.out.println(dto.getTotalPage());
+	}
+	
+}
