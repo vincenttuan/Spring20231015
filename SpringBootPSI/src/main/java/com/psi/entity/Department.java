b394e96f8fea24cc47e4249024801e93b6da799e
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 13:36:34 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/psi/entity/Department.java
@@ -16,13 +16,13 @@ import lombok.Setter;
 public class Department {
 	
 	// @Id: 主鍵
-	@Id 
 	// @GeneratedValue: 自動產生值
 	// strategy = GenerationType.IDENTITY : 自動增長, 適用於 MySQL
 	// strategy = GenerationType.SEQUENCE : 自動增長, 適用於 Oracle
 	// strategy = GenerationType.TABLE : 自動增長, 適用於所有資料庫
 	// GenerationType.TABLE 會建立一個資料表, 用來紀錄目前的 id 值, 並且鎖定該筆資料, 避免同時新增時發生重複
 	// strategy = GenerationType.AUTO : 自動增長, 由系統自動選擇適合的策略
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; // 部門序號
 	
