0c315a3ae0ea2fe7694b2e0f4089d1a3ba1e6e91
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-07 13:15:05 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/psi/SpringBootPsiApplication.java
@@ -0,0 +1,13 @@
+package com.psi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPsiApplication.class, args);
+	}
+
+}
