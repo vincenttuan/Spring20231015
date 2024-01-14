465a1eb56e9ac78318eb9331429b45e263c012f9
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-14 15:12:39 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/service/EmployeeService.java
+++ SpringBootPSI/src/main/java/com/psi/service/EmployeeService.java
@@ -1,5 +1,32 @@
 package com.psi.service;
 
-public class EmployeeService {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.psi.model.dto.EmployeeDto;
+import com.psi.model.po.Employee;
+import com.psi.repository.DepartmentRepository;
+import com.psi.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	// 新增
+	public void add(EmployeeDto employeeDto) {
+		// dto 轉 po
+		Employee employee = modelMapper.map(employeeDto, Employee.class);
+		// 儲存
+		employeeRepository.save(employee);
+	}
+	
 }
