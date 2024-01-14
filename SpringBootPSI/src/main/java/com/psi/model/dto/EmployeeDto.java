9343349326b1ff382144c856947342d25ac3c3ad
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-14 14:59:29 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/model/dto/EmployeeDto.java
+++ SpringBootPSI/src/main/java/com/psi/model/dto/EmployeeDto.java
@@ -1,5 +1,16 @@
 package com.psi.model.dto;
 
-public class EmployeeDto {
+import com.psi.model.po.Department;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDto {
+	
+	private Long id; // 員工序號
+	private String name; // 員工姓名
+	private Department department; // 所屬部門
+	
 }
