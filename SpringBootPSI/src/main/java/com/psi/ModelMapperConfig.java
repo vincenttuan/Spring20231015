d9192b051869799230b8c8a4762773ae3f2225d5
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-14 14:19:28 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/ModelMapperConfig.java
+++ SpringBootPSI/src/main/java/com/psi/ModelMapperConfig.java
@@ -3,6 +3,7 @@ package com.psi;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class ModelMapperConfig {
@@ -10,5 +11,13 @@ public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	// 將 HTML Form(POST) + _method=PUT     -> @PutMapping
+	// 將 HTML Form(POST) + _method=DELETE  -> @DeleteMapping
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+		return filter;
 	}
 }
