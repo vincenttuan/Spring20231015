b00f77a9366f510b1b86e36129218b38d61096f0
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-14 16:06:08 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/controller/EmployeeController.java
+++ SpringBootPSI/src/main/java/com/psi/controller/EmployeeController.java
@@ -1,5 +1,19 @@
 package com.psi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * URL 路徑設計
+ * GET  /employee/?page0&size=5 分頁查詢
+ * GET  /employee/edit/{id} 單筆查詢(修改用)
+ * POST /employee/ 新增
+ * PUT  /employee/{id} 修改
+ * GET  /employee/delete/{id} 刪除
+ * */
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
 }
