31d4bd1e5ccb8e89059bb7ed779bfe7d124bb9cb
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-21 13:15:18 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/psi/model/dto/SupplierDto.java
@@ -0,0 +1,12 @@
+package com.psi.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SupplierDto {
+	private Long id; // 供應商序號 from po
+	private String name; // 供應商名稱 from po
+	
+}
