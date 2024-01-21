9ee84bd85a98cfeff96e39568b7318520202f133
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-21 15:25:58 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/psi/model/po/Product.java
@@ -0,0 +1,31 @@
+package com.psi.model.po;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 商品序號
+	
+	@Column
+	private String name; // 商品名稱
+	
+	@Column
+	private Integer cost; // 商品成本
+	
+	@Column
+	private Integer price; // 商品定價
+	
+}
