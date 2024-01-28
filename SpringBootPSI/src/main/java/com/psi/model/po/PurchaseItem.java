637d3e7861abd55b93f23da6ee0e7ba451ee3785
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-28 13:16:52 +0800

    java

--- SpringBootPSI/src/main/java/com/psi/model/po/PurchaseItem.java
+++ SpringBootPSI/src/main/java/com/psi/model/po/PurchaseItem.java
@@ -1,5 +1,35 @@
 package com.psi.model.po;
 
-public class PurchaseItem {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "purchase_item")
+@Getter
+@Setter
+public class PurchaseItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 採購明細序號
+	
+	@Column
+	private Integer amount; // 採購數量
+	
+	@JoinColumn(name = "purchase_id")
+	@ManyToOne
+	private Purchase purchase;
+	
+	@JoinColumn(name = "product_id")
+	@ManyToOne
+	private Product product;
+	
 }
